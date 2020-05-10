a335eefb0e6ab195b7e8535f5a87e2fe9681f6a5
nom30523 <60598776+nom30523@users.noreply.github.com>
2020-05-10 16:48:11 +0900

    integer,float,enumratorオブジェクト

---
+++ Rubyの基礎/integer.rb
@@ -0,0 +1,26 @@
+# integer, float, enumeratorオブジェクトについて
+p 1.class
+p 1.5.class
+p 1 + 1
+p 2 - 1
+p 100 * 1.08
+p 4 / 2
+
+p (1 + 1.0).class
+
+p 5 % 2
+p 5 ** 2
+
+p 1.5.round
+p 1.4.round
+
+p 1.1.ceil
+p 1.1.floor
+
+p 3.times
+
+3.times do |i|
+  puts i
+end
+
+3.times { |num| puts num }
