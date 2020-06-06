6f3150236efe43c8eecf64d7cee12c440fd75b9a
nom30523 <60598776+nom30523@users.noreply.github.com>
2020-06-06 15:51:36 +0900

    stopwatch init

--- JSアプリ/MyOmikuji/js/main.js
+++ JSアプリ/MyOmikuji/js/main.js
@@ -9,7 +9,7 @@
     // btn.textContent = results[Math.floor(Math.random() * results.length)];
     const n = Math.random();
     if (n < 0.05) {
-      btn.textContent = '大吉' // 5%
+      btn.textContent = '大吉'; // 5%
     } else if (n < 0.2) {
       btn.textContent = '中吉'; // 15%
     } else {
